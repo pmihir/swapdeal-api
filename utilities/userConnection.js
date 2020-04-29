76f09d35a42969bbf6b49f3d776675a28c611e73
Mihir Panchal <pmihir98512@gmail.com>
2020-04-29 22:19:38 +0530

    DB Configured to cloud and resolveed issue of loosely coupled data

--- utilities/userConnection.js
+++ utilities/userConnection.js
@@ -1,19 +1,22 @@
 const {Schema} = require("mongoose");
 const Mongoose = require("mongoose");
 const crypto = require("crypto");
+const MongoClient = require("mongodb").MongoClient;
+const uri = "mongodb+srv://Mihir:Mihir%401234@cluster0-kvs47.mongodb.net/SwapDeal?retryWrites=true&w=majority";
 const jwt = require('jsonwebtoken');
 Mongoose.Promise = global.Promise;
 Mongoose.set('useCreateIndex',true);
 // Mongoose.set('userCreateIndex', true);
-const url = "mongodb://localhost:27017/SwapDeal";
+// const url = "mongodb://localhost:27017/SwapDeal";
+// mongodb+srv://Mihir:<password>@cluster0-kvs47.mongodb.net/test?retryWrites=true&w=majority
 
 const userSchema = Schema({
-    firstname:String,
-    lastname:String,
-    username:String,
-    email: {type:String , unique:true},
-    phonenumber:Number,
-    password:String,
+    firstname:{type : String , required : true},
+    lastname:{type : String , required : true},
+    username:{type : String , required : true},
+    email: {type:String , unique:true ,required : true},
+    phonenumber:{type : String , required : true},
+    password:{type : String , required : true},
 }, {collection : "Users", timestamp:true})
 
 const resettokenSchema = Schema({
@@ -28,9 +31,11 @@ const resettokenSchema = Schema({
 let userCollection = {};
 
 userCollection.getUserCollection = () =>{
-    return Mongoose.connect(url, {useNewUrlParser:true}).then((database)=>{
+    return Mongoose.connect(uri, {useNewUrlParser:true}).then((database)=>{
+        console.log("connected");
         return database.model('Users', userSchema)
     }).catch((error)=>{
+        console.log(error);
         let err = new Error("Could not connect to Database");
         err.status = 500;
         throw err;
@@ -38,7 +43,7 @@ userCollection.getUserCollection = () =>{
 }
 
 userCollection.getResetTokenCollection = () =>{
-    return Mongoose.connect(url, {useNewUrlParser:true}).then((database)=>{
+    return Mongoose.connect(uri, {useNewUrlParser:true}).then((database)=>{
         return database.model('ResetPassword', resettokenSchema)
     }).catch((error)=>{
         let err = new Error("Could not connect to Database");
